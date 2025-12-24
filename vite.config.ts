a9482d1a8dae5d558ef3788f7b1e199a5cd080db
J404Simpson <jasonjamessimpson@gmail.com>
2025-12-24 13:46:28 +1300

    Deployment error workthrough

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.',
   plugins: [react()],
+  base: './',
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
     host: '0.0.0.0',
