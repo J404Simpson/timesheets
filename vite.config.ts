ee23e5692f7b4f314e7f4223a514d8cb6a8e6c14
J404Simpson <jasonjamessimpson@gmail.com>
2025-12-24 15:06:24 +1300

    Deployment error workthrough

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
     host: '0.0.0.0',
+    allowedHosts: ['timesheets.veryanmed.com'],
   },
 });
